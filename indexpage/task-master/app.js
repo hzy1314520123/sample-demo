f211be4e3e7c6cb1d2267b4c6e36548193bc2845
hzy1314520 <1780548815@qq.com>
2017-10-23 12:34:27 +0800

    阿斯蒂芬

---
+++ indexpage/task-master/app.js
@@ -0,0 +1,9 @@
+angular.module('routingDemoApp',['ngRoute']);
+
+module.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/',{template:'这是首页页面'})
+        .when('/computers',{template:'这是电脑分类页面'})
+        .when('/printers',{template:'这是打印机页面'})
+        .otherwise({redirectTo:'/'});
+}]);
